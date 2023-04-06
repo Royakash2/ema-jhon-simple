a0b5191b5db213e97e8661af5351839885f9dcee
Royakash2 <yahmeakash@gmail.com>
2023-04-07 03:07:52 +0600

    implemented delete cart

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './component/shoping/Shop';
+import Home from './component/Layout/Home';
+import Oders from './component/Oders/Oders';
+import Inventory from './component/Inventory/Inventory';
+import LogIn from './component/LogIn/LogIn';
+import cartProductsLoader from './Loaders/CartProductsLoader';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Oders></Oders>,
+        loader: cartProductsLoader
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <LogIn></LogIn>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
